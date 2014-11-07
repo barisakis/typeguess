893e47aa44d6047834f01fb193b0dc61c7356343
Baris Akis <barisakis@Bariss-MacBook-Pro-3.local>
2014-11-07 13:23:15 -0800

    clean the input box

--- TypeGuessData/AppDelegate.swift
+++ TypeGuessData/AppDelegate.swift
@@ -5,14 +5,13 @@
 //  Created by Baris Akis on 11/6/14.
 //  Copyright (c) 2014 Baris Akis. All rights reserved.
 //
-
+import CoreMotion
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
